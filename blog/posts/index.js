76218dcbf74f55bc559bd7c23338cd486fd00460
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 11:40:21 +0100

    Added event emitter for post service

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const fs = require("fs");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get("/posts", (req, res) => {
   res.send(postList);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
 
   const { title } = req.body;
@@ -22,6 +23,11 @@ app.post("/posts", (req, res) => {
   postList[id] = { id, title };
 
   res.status(201).send(postList[id]);
+
+  await axios.post("http://localhost:4005/event", {
+    type: "POST_CREATED",
+    data: postList[id],
+  });
 });
 
 app.listen(4000, () => console.log("Listening on 4000"));
