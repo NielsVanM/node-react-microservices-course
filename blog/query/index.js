b85d56d6ed88ab4f032c1a87364c4e7dbf14e805
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 12:58:45 +0100

    Created query service

---
+++ blog/query/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", async (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  switch (type) {
+    case "POST_CREATED":
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    case "COMMENT_CREATED":
+      const { commentId, content, postId } = data;
+      const post = posts[postId];
+      post.comments.push({ commentId, content });
+      posts[postId] = post;
+      break;
+  }
+
+  console.log(posts);
+
+  res.sendStatus(200);
+});
+
+app.listen(4002, () => console.log("Running on port 4002"));
