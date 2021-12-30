e2b8d9d8938354a921de7228e04def38b941da0e
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 14:50:39 +0100

    Swapped switched statement for if statement due to variable name clash

--- blog/query/index.js
+++ blog/query/index.js
@@ -15,17 +15,25 @@ app.get("/posts", async (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  switch (type) {
-    case "POST_CREATED":
-      const { id, title } = data;
-      posts[id] = { id, title, comments: [] };
-      break;
-    case "COMMENT_CREATED":
-      const { commentId, content, postId, status } = data;
-      const post = posts[postId];
-      post.comments.push({ commentId, content, status });
-      posts[postId] = post;
-      break;
+  if (type === "POST_CREATED") {
+    let { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "COMMENT_CREATED") {
+    let { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+    posts[postId] = post;
+  }
+
+  if (type === "COMMENT_UPDATED") {
+    let { id, content, postId, status } = data;
+    console.log(data);
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id == id);
+    comment.content = content;
+    comment.status = status;
   }
 
   res.sendStatus(200);
