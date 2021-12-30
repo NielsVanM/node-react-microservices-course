ee02382e13ec5f6f9bcb4b8d071c0bce40c0eb1d
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 15:33:34 +0100

    Load old events on query service  boot

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +16,20 @@ app.get("/posts", async (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
+  handleEvent(type, data);
+
+  res.sendStatus(200);
+});
+
+app.listen(4002, async () => {
+  console.log("Running on port 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+  res.data.forEach((event) => handleEvent(event.type, event.data));
+  console.log(`Processed ${res.data.length} old events`);
+});
+
+function handleEvent(type, data) {
   if (type === "POST_CREATED") {
     let { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -29,14 +44,9 @@ app.post("/events", (req, res) => {
 
   if (type === "COMMENT_UPDATED") {
     let { id, content, postId, status } = data;
-    console.log(data);
     const post = posts[postId];
     const comment = post.comments.find((comment) => comment.id == id);
     comment.content = content;
     comment.status = status;
   }
-
-  res.sendStatus(200);
-});
-
-app.listen(4002, () => console.log("Running on port 4002"));
+}
