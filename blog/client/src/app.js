e7583d64daa1bc607eddb5662aca34ae3269f929
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 10:09:53 +0100

    Created Post List

--- blog/client/src/app.js
+++ blog/client/src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PostCreate } from "./components/post_create";
+import { PostList } from "./components/post_list";
 
 export const App = () => {
   return (
@@ -7,6 +8,10 @@ export const App = () => {
       <h1>Create Post</h1>
 
       <PostCreate />
+
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
