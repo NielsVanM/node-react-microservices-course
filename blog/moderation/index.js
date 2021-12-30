ef5f85a76b72791083f902e563f5494df1cb6e5e
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 14:50:56 +0100

    Fixed wrong word filtering & cleanup

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -5,14 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-const commentsToModerate = {};
-
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   switch (type) {
     case "COMMENT_CREATED":
-      const status = data.content.includes("organge") ? "rejected" : "approved";
+      const status = data.content.includes("orange") ? "rejected" : "approved";
 
       await axios.post("http://localhost:4005/events", {
         type: "COMMENT_MODERATED",
