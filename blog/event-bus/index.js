eed391a1c6d97cb30f5947c645ca0a3b5ec55150
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-30 15:33:15 +0100

    Implemented event history for event bus

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -2,16 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
+const eventHistory = [];
+
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/events", (req, res) => {
+  res.send(eventHistory);
+});
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  eventHistory.push(event);
+
+  axios
+    .post("http://localhost:4000/events", event)
+    .catch((error) => console.log(error));
+  axios
+    .post("http://localhost:4001/events", event)
+    .catch((error) => console.log(error));
+  axios
+    .post("http://localhost:4002/events", event)
+    .catch((error) => console.log(error));
+  axios
+    .post("http://localhost:4003/events", event)
+    .catch((error) => console.log(error));
 
   res.send({ status: "ok" });
 });
