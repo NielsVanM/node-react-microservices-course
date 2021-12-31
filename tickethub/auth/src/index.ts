b2ebe3d05a6cb2028da3df34f7fda1784b7eee99
Niels van Marion <niels.van.marion@quicknet.nl>
2021-12-31 12:56:29 +0100

    Added auth service currentuser handler dummy handler

--- tickethub/auth/src/index.ts
+++ tickethub/auth/src/index.ts
@@ -4,4 +4,8 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi");
+});
+
 app.listen(3000, () => console.log("Started service on port 3000"));
